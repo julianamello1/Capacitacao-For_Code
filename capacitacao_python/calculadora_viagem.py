43416bee6faf87e50639cf2fa30b925ebf49c95f
julianamello1 <jssmello06@gmail.com>
2026-05-14 15:40:23 -0300

    primeiro commit push da lógica, não finalizado ainda

--- capacitacao_python/calculadora_viagem.py
+++ capacitacao_python/calculadora_viagem.py
@@ -24,12 +24,39 @@ destino = input("Qual é o destino da sua viagem? ")
 
 custo_da_passagem = float(input("Qual é o valor da passagem, em reais? "))
 
-custo_da_hospedagem = float(input("Qual é o custo da hospedagem, em reais? "))
+custo_diario_da_hospedagem = float(input("Qual é o custo da hospedagem, em reais? "))
 
 qntd_dias = int(input("Qual a duração da viagem, em dias? "))
 
 # organização e checagem dos dados inseridos:
 
 print(f"\nPerfeito! Segundo seus dados inseridos, você fará uma viagem para {destino}, durante {qntd_dias} dias.")
-print(f"\nSeu orçamento é de R$ {orcamento_disponivel}. O valor da passagem é de R$ {custo_da_passagem} e o da hospedagem, R$ {custo_da_hospedagem}.")
+print(f"\nSeu orçamento é de R$ {orcamento_disponivel}. O valor da passagem é de R$ {custo_da_passagem} e o da diária da hospedagem, R$ {custo_diario_da_hospedagem}.")
 
+
+## lógica e cálculos
+
+# converter moeda (função) = valor em reais x 6.10:
+def conversao_real_euro(valor_em_reais):
+    return valor_em_reais * 6.10
+
+# calcular hospedagem (função) = valor da hospedagem (diário) x quantidade de dias:
+def calculo_hospedagem(valor_hospedagem, qntd_dias):
+    return valor_hospedagem * qntd_dias
+
+# calcular custo total (função) = valor passagem + total hospedagem:
+def calculo_custo_total(valor_passagem, valor_total_hospedagem):
+    return valor_total_hospedagem + valor_passagem
+
+# validação do orçamento (função) = se custo_total for <= orçamento_disponivel, POSSÌVEL
+# se custo_total > orçamento_disponível, NÃO POSSÌVEL
+def validacao_orcamento(custo_total, orcamento_disponivel):
+    if(custo_total <= orcamento_disponivel):
+        print("\n\tDe acordo com os custos totais e o orçamento disponível, sua viagem é viável!")
+        return True
+    else:
+        print(f"\n\tInfelizmente, por conta do orçamento, essa viagem não seria possível :( )")
+        return False
+
+# status final da viagem (função) = viavel se orçamento valido && qntd dias > 0
+# calculo do quanto sobrará (orçamento - custo) ou quanto faltará (custo - orçamento)
