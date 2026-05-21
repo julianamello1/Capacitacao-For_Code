58c01a5c167ca9edccf36e55d73f113f45ff2a9e
julianamello1 <jssmello06@gmail.com>
2026-05-20 23:57:56 -0300

    adicionei as funções

--- capacitacao_python/simulador_tcg.py
+++ capacitacao_python/simulador_tcg.py
@@ -29,3 +29,21 @@ pontos_ataque_m1 = int(input("Pontos de ataque do monstro 1: "))
 nome_m2 = input("\nNome do monstro 2: ")
 pontos_vida_m2 = int(input("\nPontos de vida do monstro 2 (HP): "))
 pontos_ataque_m2 = int(input("Pontos de ataque do monstro 2: "))
+
+
+## funções
+
+# função de atacar
+def atacar(nome_atacante, ataque, nome_defensor, hp_defensor):
+    dano = pontos_vida_m1 - pontos_ataque_m2
+    vida_atualizada = pontos_vida_m1 - dano
+    print(f"\n\t{nome_atacante} atacou {nome_defensor} e causou {dano} de dano!!!!")
+    print(f"\n\t\tVida atual de {nome_defensor}: {vida_atualizada} HP")
+    return vida_atualizada
+
+# função que exibe placar
+def exibir_placar(nome_m1, pontos_vida_m1, nome_m2, pontos_vida_m2):
+    print(f"\n\tVida de {nome_m1}: {pontos_vida_m1} HP")
+    print(f"\n\tVida de {nome_m2}: {pontos_vida_m2} HP")
+    return
+
